a8e21ddccf5d5d1e178e8479a7ce83e130eab87f
fuieaw <fuiefhgj9@gmail.com>
2025-11-19 14:30:10 +0900

    Add files via upload

---
+++ 조별과제BankApplication/Account.java
@@ -0,0 +1,30 @@
+package ch06.조별과제BankApplication;
+
+public class Account {
+	private String number;     
+    private String owner;   
+    private int money;   
+    public Account(String number, String owner, int money) {
+        this.number=number;
+        this.owner=owner;
+        this.money=money;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number=number;
+    }
+    public String getOwner() {
+        return owner;
+    }
+    public void setOwner(String owner) {
+        this.owner=owner;
+    }
+    public int getMoney() {
+        return money;
+    }
+    public void setMoney(int money) {
+        this.money=money;
+    }
+}
